43887bd5076b378e30cc13c703b81a7b9570726e
conejofoz <conejofoz@gmai.com>
2025-01-14 13:20:08 -0300

    Link - forma bem simples

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export const About = ()=>{
     return (
         <div>
             ABOUT
             <hr />
             <ul>
-                <li>João</li>
-                <li>Juanito</li>
+                <li><Link to="/sobre/joao">João</Link></li>
+                <li><Link to="/sobre/juanito">Juanito</Link></li>
             </ul>
 
         </div>
