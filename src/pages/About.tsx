71cec9014b84ed9761dfaf1e0a492860c8bdbabf
conejofoz <conejofoz@gmai.com>
2025-01-14 17:12:51 -0300

    modificando a query sem perder os parametros

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export const About = ()=>{
+    const [searchParams, setSearchParams ] = useSearchParams();
+
+    const setOrder = (order: 'asc' | 'desc') =>{
+        searchParams.set('order', order);
+        setSearchParams(searchParams);
+    }
+    
     return (
         <div>
+            Filtro: {searchParams.get('filter')}
+            Ordem: {searchParams.get('order')}
+            <hr />
+            <button onClick={()=>setOrder('asc')}>Asc</button>
+            <button onClick={()=>setOrder('desc')}>Desc</button>
+            <hr />
             ABOUT
             <hr />
             <ul>
