561aa8bb9852ce6086621c31eb468d767eca28aa
conejofoz <conejofoz@gmai.com>
2025-01-14 15:45:02 -0300

    Redicionamento de rotas

--- src/pages/AboutItem.tsx
+++ src/pages/AboutItem.tsx
@@ -1,11 +1,23 @@
-import { useParam, useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 export const AboutItem = ()=>{
     const params = useParams();
+    const navigate = useNavigate()
+
+    const handleBackButton = ()=>{
+        navigate(-1); //volta uma página do histórico
+    }
+
+    const handleHomeButton = ()=>{
+        navigate('/')
+    }
 
     return (
         <div>
             Página sobre de: {params.slug}
+            <hr />
+            <button onClick={handleBackButton} className='mt-4'>Voltar</button>
+            <button onClick={handleHomeButton} className='mt-4'>Home</button>
         </div>
     )
 }
