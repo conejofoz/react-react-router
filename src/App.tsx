0b0136b6ea42778d37a068e46c3218998225ac31
conejofoz <conejofoz@gmai.com>
2025-01-13 23:23:50 -0300

    adicionando rotas de forma básica

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { Routes, Route } from 'react-router-dom'; 
+
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+
 const App = ()=>{
   return (
-    <div>...</div>
+    <div>
+      <header>
+        <h1>Título do site</h1>
+      </header>
+      <hr />
+      <div className="py-4">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/sobre' element={<About />} />
+        </Routes>
+      </div>
+      <hr />
+      <footer>
+        Rodapé bla bla bla
+      </footer>
+    </div>
   )
 }
 
