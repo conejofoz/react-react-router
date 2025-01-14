f6479a48b9d94981b94ba44ea74da4f75e1f52a8
conejofoz <conejofoz@gmai.com>
2025-01-14 17:53:53 -0300

    Rotas em objetos assim como eu uso no vueJS

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'; 
+import { Routes, Route, useRoutes } from 'react-router-dom'; 
 
 import { Home } from '../pages/Home';
 import { About } from '../pages/About';
@@ -8,7 +8,22 @@ import { RequireAuth } from '../auth/RequireAuth';
 import { Login } from '../pages/Login';
 
 export const MainRoutes = ()=>{
-    return (
+
+    return useRoutes([
+        {path: '/', element: <Home />},
+        {path: '/sobre', element: <RequireAuth><About /></RequireAuth>},
+        {path: '/sobre/:slug', element: <AboutItem />},
+        {path: '/login', element: <Login />},
+        {path: '*', element: <NotFound />},
+    ])
+
+    /* 
+    Obs: se quiser usar no App é só criar uma constante e atribuir a esse objeto useRoutes e usar no template assim:
+    const routes = useRoutes([objstos acima])
+    { routes }
+     */
+
+    /* return (
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/sobre' element={<RequireAuth><About /></RequireAuth>} />
@@ -16,5 +31,5 @@ export const MainRoutes = ()=>{
           <Route path='/login' element={<Login />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
-    )
+    ) */
 }
